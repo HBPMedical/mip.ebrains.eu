da3e58920075a1ee4e1852b5c6985b4ae394af9d
Manuel Spuhler <manuel.spuhler@gmail.com>
2020-05-03 11:39:02 +0200

    Cleanup

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ source .env
 stop() {
     echo -e "\nRemoving previous services...\n"
     docker-compose -f docker-compose.yml --project-name mip-ebrains-eu down
+
     docker_compose_down=$?
     if [[ ${docker_compose_down} -ne 0 ]]; then
         echo -e "\nAn error has occurred while removing services and networks.Exiting.." >&2
@@ -32,6 +33,7 @@ main() {
 
         echo -e "\nRunning Production Environnement\n"
         docker-compose -f docker-compose.yml --project-name mip-ebrains-eu up -d
+
         docker_compose_up=$?
         if [[ ${docker_compose_up} -ne 0 ]]; then
             echo -e "\nAn error has occurred while deploying services.Exiting.." >&2
--- develop.sh
+++ develop.sh
@@ -8,6 +8,7 @@ source .env
 stop() {
     echo -e "\nRemoving previous services...\n"
     docker-compose -f docker-compose-dev.yml --project-name dev-mip-ebrains-eu down
+
     docker_compose_down=$?
     if [[ ${docker_compose_down} -ne 0 ]]; then
         echo -e "\nAn error has occurred while removing services and networks.Exiting.." >&2
@@ -25,6 +26,7 @@ main() {
 
         echo -e "\nRunning Development Environnement\n"
         docker-compose -f docker-compose-dev.yml --project-name dev-mip-ebrains-eu up
+
         docker_compose_up=$?
         if [[ ${docker_compose_up} -ne 0 ]]; then
             echo -e "\nAn error has occurred while deploying services.Exiting.." >&2
