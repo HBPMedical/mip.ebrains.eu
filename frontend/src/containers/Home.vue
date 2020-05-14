ac5b2603d599d4aeca6e9ad3852cdd78e4e03630
Manuel Spuhler <manuel.spuhler@gmail.com>
2020-05-14 14:49:01 +0200

    Navigation Header, breadcrumb draft

--- frontend/src/containers/Home.vue
+++ frontend/src/containers/Home.vue
@@ -1,11 +1,18 @@
 <template>
-  <b-container>
-    <b-row>
-      <b-col>
-        <Tabs :articles="articles"></Tabs>
-      </b-col>
-    </b-row>
-  </b-container>
+  <div>
+    <b-container
+      fluid
+      class="header d-flex align-items-center justify-content-center"
+    >
+      <div class="header-card">
+        <h1>The Medical Informatics Platform</h1>
+        <p>Secure federated analysis on clinical data</p>
+      </div>
+    </b-container>
+    <b-container class="content">
+      <Tabs :articles="articles"></Tabs>
+    </b-container>
+  </div>
 </template>
 
 <script>
@@ -41,3 +48,31 @@ export default {
   },
 };
 </script>
+
+<style>
+.header {
+  background: no-repeat top/100% url("~@/assets/system-2660914_1920.jpg");
+  height: 20rem;
+}
+
+.header-card {
+  width: 960px;
+  text-align: center;
+  color: white;
+}
+
+.header-card h1 {
+  font-size: 4rem;
+  font-weight: bolder;
+}
+
+.header-card p {
+  font-size: 1.5rem;
+  font-weight: bolder;
+}
+
+.content {
+  background-color: rgba(255, 255, 255, 0.432);
+  margin-top: 2rem;
+}
+</style>
