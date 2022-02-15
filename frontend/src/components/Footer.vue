326ebe5ea086cd8e2f08d9d4d8c0d39b1a10d9e6
stevereis <stevereis93@gmail.com>
2022-02-15 15:30:57 +0100

    WIP: Ebrains logo size

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,16 +1,17 @@
 <template>
   <b-navbar class="footer" toggleable="sm" type="white" variant="white">
     <b-collapse id="nav-text-collapse" is-nav>
-      <b-navbar-nav>
+      <b-navbar-nav class="d-none d-md-flex">
         <b-nav-text>Copyright © 2022 EBRAINS. All rights reserved.</b-nav-text>
       </b-navbar-nav>
     </b-collapse>
 
-    <b-navbar-nav class="footer-logos ml-auto">
+    <div class="footer-logos ml-auto">
       <img src="~@/assets/eu-fund-logo.png" alt="EU Funding logo" class="img-fluid" />
       <img src="~@/assets/hbp-logo.png" alt="HBP logo" class="img-fluid" />
-      <img src="~@/assets/ebrains-logo.png" alt="Ebrains logo" class="img-fluid" />
-    </b-navbar-nav>
+      <img src="~@/assets/ebrains-logo.png" alt="Ebrains logo" class="img-fluid d-none d-md-inline-block" />
+      <img src="~@/assets/ebrains-logo-sm.png" alt="Ebrains logo" class="img-fluid d-xs-inline-block d-sm-inline-block d-md-none" />
+    </div>
   </b-navbar>
 </template>
 
@@ -20,13 +21,13 @@
   background-color: #f5f5f5;
   .footer-logos {
     height: 45px;
+    img {
+      max-height: 100%;
+      margin-right: 0.5rem;
+    }
   }
 }
 
-img {
-  margin-right: 1rem;
-  max-height: 60px;
-}
 .navbar-text {
   font-size: 0.8rem;
 }
