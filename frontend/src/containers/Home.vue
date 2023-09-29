bf55823b632109e5478532de54740eefff2528b6
Manuel Spuhler <manuel.spuhler@chuv.ch>
2023-09-29 11:43:50 +0200

    feat: new federation data

--- frontend/src/containers/Home.vue
+++ frontend/src/containers/Home.vue
@@ -132,6 +132,31 @@ export default {
   data() {
     return {
       federations: [
+      {
+          title: "Public MIP",
+          description:
+            "The PUBLIC-MIP installed on the EBRAINS RI and available via EBRAINS accreditation on the EBRAINS website. EBRAINS users can explore the service with no requirement for installation or download. Analyse online all data placed in the public MIP and test the available MIP analytical tools. New Public Dataset available: The Leenaards Memory Centre (CLM) in Lausanne, Switzerland, made a substantial dataset of anonymised real world patient records (1032 records) available on the MIP. This provides a valuable data source in the field of dementia to the research community, which is otherwise very hard to access. Furthermore, synthetic data, merely intended for exploring the system and testing its analytical capabilities covering different pathologies, are available for the community. What users can test and do on the Public MIP: Experiment on synthetic and public datasets in Dementia, Epilepsy, Mental Health and TBI; select datasets, variables and co-variables to define the experimental parameters; choose between available algorithms (15 algorithms ranging from descriptive statistics, parametric statistics to supervised and unsupervised machine learning are built into the MIP); create, safe and share experiments to perform statistical analysis. Access the MIP Guide and Training Materials.",
+          nbRecords: 521913,
+          link: "https://hbpmip.link",
+        },
+        {
+          title: "FERES - Federating European Registries for Stroke",
+          description:
+            "National Stroke Registries have played an essential role in accelerating stroke research that affects the advancement and delivery of effective, high-quality care. One of the main objectives of FERES is the development of a framework to impulse the re-use of health data collected in European National Stroke Registries and Electronic Health Records (EHR) in a fully General Data Protection Regulation (GPDR)-compliant way.",
+          nodes: [
+            "NKUA - National and Kapodistrian University of Athens",
+            "UB - University of Basel",
+            "",
+            "In process:",
+            "VASCage - VASCage GmbH - Austrian stroke registry",
+            "Careggi University Hospital / Hospital Policlinico Umberto I",
+            "CHUL - Hospital Center University of Lille",
+            "INAS - Irish National Audit of Stroke",
+            "AAU - Aalborg University "
+          ],
+          nbRecords: 110413,
+          link: "https://stroke.hbpmip.link",
+        },
         {
           title: "Dementia",
           description:
@@ -143,6 +168,32 @@ export default {
           ],
           nbRecords: 6348,
           link: "https://dementia.hbpmip.link/",
+        },
+        {
+          title: "French Epilepsy Network – REPOMSE",
+          description:
+            "National Stroke Registries have played an essential role in accelerating stroke research that affects the advancement and delivery of effective, high-quality care. One of the main objectives of FERES is the development of a framework to impulse the re-use of health data collected in European National Stroke Registries and Electronic Health Records (EHR) in a fully General Data Protection Regulation (GPDR)-compliant way.",
+          nbRecords: 1072,
+          nodes: [
+            "AP_HM - Centre Hospitalier Universitaire de Marseille",
+            "CHU Nice - Centre Hospitalier Universitaire de Nice",
+            "AP_HP - Centre Hospitalier Universitaire de la Pitié-Salpêtrière",
+            "GHU PARIS - Centre Hospitalier Sainte Anne",
+            "CHU Rennes - Centre Hospitalier Régional et Universitaire de Rennes",
+            "CHRU Strasbourg - Centre Hospitalier Universitaire de Strasbourg",
+            "CHU Toulouse - Centre Hospitalier Universitaire de Toulouse",
+            "CHRU Tours - Centre Hospitalier Universitaire de Tours",
+            "CHRU Nancy - Centre Hospitalier Universitaire de Nancy",
+            "CHU GRENOBLE - Centre Hospitalier Universitaire Michallon - Département de Neurologie, Epilepsie",
+            "CHU Lille - Hospital Centre University of Lille",
+            "HCL - Hospices Civils de Lyon - Hôpital Neurologique",
+            "",
+            "In process :",
+            "LA TEPPE - Centre médical de la Teppe - Tain l'hermitage",
+            "CHU BORDEAUX - Centre Hospitalier Universitaire Pellegrin Tripode",
+          "CHU_MONTPELLIER - Centre Hospitalier Universitaire Gui de Chauliac"
+          ],
+          link: "https://stroke.hbpmip.link",
         },
         {
           title: "Traumatic Brain Injury (TBI)",
@@ -179,6 +230,17 @@ export default {
           link: "https://epilepsy.hbpmip.link",
         },
         {
+          title: "Mental Health",
+          description:
+            "The federation aims at exploring large-scale Mental Health datasets including clinical signs and symptoms, together with structured neuropsychological assessments and brain imaging data. Data are derived from a pool of individuals in a longitudinal fashion as part of the IMAGEN database, collected by the Imagen consortium from over 2000 adolescents and their parents.",
+          nodes: [
+            "Universitätsklinikum Aachen (Aachen, Germany)",
+            "CHUV (Centre Hospitalier Universitaire Vaudois, Lausanne, Switzerland)",
+          ],
+          nbRecords: 7300,
+          link: "https://mentalhealth.hbpmip.link/",
+        },
+        {
           title: "NEURO Cohort",
           description: `The NEURO Cohort project gathers a European network of 40 sites dedicated to neurodegenerative (ND) research, which are committed to the harmonisation and sharing of ten routinely-collected variables. These sites include universities, research centres and memory clinics, representing members of the original EPAD consortium and the extended IMI NEURONET researcher network. The goal of the NEURO Cohort is to provide a dataset that can be utilised for feasibility analyses and European cohort characterisation, to accelerate and streamline initiation and recruitment of research projects and studies.
 
@@ -191,24 +253,6 @@ The pilot has entailed the production of the NEURO Cohort common data model (CDM
             "Oxford University, Department of Psychiatry, Oxford, UK",
           ],
           link: "https://neurocohort.hbpmip.link",
-        },
-        {
-          title: "Public MIP",
-          description:
-            "The PUBLIC-MIP installed on the EBRAINS RI and available via EBRAINS accreditation on the EBRAINS website. EBRAINS users can explore the service with no requirement for installation or download. Analyse online all data placed in the public MIP and test the available MIP analytical tools. The public MIP only contains synthetic data, merely intended for exploring the system and testing its analytical capabilities. What users can test and do on the Public MIP: Experiment on synthetic and public datasets in Dementia, Epilepsy, Mental Health and TBI; select datasets, variables and co-variables to define the experimental parameters; choose between available algorithms (15 algorithms ranging from descriptive statistics, parametric statistics to supervised and unsupervised machine learning are built into the MIP); create, safe and share experiments to perform statistical analysis. Access MIP Training Materials.",
-          nbRecords: 521913,
-          link: "https://hbpmip.link",
-        },
-        {
-          title: "Mental Health",
-          description:
-            "The federation aims at exploring large-scale Mental Health datasets including clinical signs and symptoms, together with structured neuropsychological assessments and brain imaging data. Data are derived from a pool of individuals in a longitudinal fashion as part of the IMAGEN database, collected by the Imagen consortium from over 2000 adolescents and their parents.",
-          nodes: [
-            "Universitätsklinikum Aachen (Aachen, Germany)",
-            "CHUV (Centre Hospitalier Universitaire Vaudois, Lausanne, Switzerland)",
-          ],
-          nbRecords: 7300,
-          link: "https://mentalhealth.hbpmip.link/",
         },
         {
           title: "MIP Hands-on",
